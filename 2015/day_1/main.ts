fe020579b523a2ccf4c2831b1f1f2d8ddad2ead2
Ghoui <flareonverm@outlook.com>
2025-11-22 11:05:16 +0000

    day 1 finished

--- 2015/day_1/main.ts
+++ 2015/day_1/main.ts
@@ -3,9 +3,10 @@ import {readFileSync} from 'fs'
 const inputFile = readFileSync('./input', 'utf-8')
 
 function puzzleOne(input : string){
-    let currentFloor = 0;
+    let currentFloor = 0; //Current Floor Santa is on.
     for(let i = 0; i < input.length; i++){
-        if(input.charAt(i) == '('){
+        // ( goes up a stair and ) down a stair.
+        if(input.charAt(i) == '('){ 
             currentFloor += 1
         }else if(input.charAt(i) == ')'){
             currentFloor -= 1
@@ -13,4 +14,20 @@ function puzzleOne(input : string){
     }
     return currentFloor
 }
+function puzzleTwo(input: string){
+    let currentFloor = 0;
+    for(let i = 0; i < input.length; i++){
+        if(input.charAt(i) == '('){
+            currentFloor += 1
+        }else if(input.charAt(i) == ')'){
+            currentFloor -= 1
+        }
+        if(currentFloor == -1){
+            console.log("Floor is "+(i + 1)) //we plus one because that is the starting position of santa. 
+        }
+    }
+    return currentFloor
+    
+}
 console.log(puzzleOne(inputFile))
+console.log(JSON.stringify(puzzleTwo(inputFile)))
